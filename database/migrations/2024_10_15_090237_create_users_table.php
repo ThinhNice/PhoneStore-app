144d0db1a9c4be50521a57d1763cec64d5ff2216
Nguyen Huu Thinh <thinhnguyen3012002@gmail.com>
2024-10-17 13:14:52 +0700

    update

--- database/migrations/2024_10_15_090237_create_users_table.php
+++ database/migrations/2024_10_15_090237_create_users_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use function Laravel\Prompts\table;
-
 return new class extends Migration
 {
     /**
@@ -21,7 +19,23 @@ return new class extends Migration
             $table->string('email', 255)->collation('utf8mb4_general_ci')->nullable(false)->unique();
             $table->string('phone', 20)->collation('utf8mb4_general_ci')->nullable(false);
             $table->integer('role')->collation('utf8mb4_general_ci')->nullable(false)->default(0);
+            $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -31,5 +45,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
