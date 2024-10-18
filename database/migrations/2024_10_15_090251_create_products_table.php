65df39bbe3e65ba092d66902dce7efb655974cae
Nguyen Huu Thinh <tts-thinhnh@bekisoft.com>
2024-10-18 15:21:37 +0700

    update

--- database/migrations/2024_10_15_090251_create_products_table.php
+++ database/migrations/2024_10_15_090251_create_products_table.php
@@ -14,14 +14,15 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->collation('utf8mb4_general_ci')->nullable(false)->unique();
+            $table->string('slug')->unique();
             $table->unsignedBigInteger('category_id');
-            
             $table->foreign('category_id','fk_products_category')->references('id')->on('categories')->onDelete('cascade');
             $table->longText('description')->collation('utf8mb4_general_ci');
             $table->decimal('price', 20, 2)->nullable(false);
             $table->unsignedBigInteger('stock')->default(0);
             $table->tinyInteger('is_hot')->default(0);
             $table->tinyInteger('is_new')->default(0);
+            $table->boolean('status')->default(1);
             $table->timestamps();
         });
     }
